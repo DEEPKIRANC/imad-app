cc441ad2a7e3d41bc47a6d6e3bb355984d5b03ec
DEEPKIRANC <cdeepkiran14@gmail.com>
2017-08-24 21:34:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.get('/article-three',function(req,res)
 {
     res.send("article-three requested");
 });
+var counter=0;
+app.get("/counter", function (req,res) 
+{
+   counter=counter+1;
+   res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

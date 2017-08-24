b52072306dd71a18161046c32f75e8a9345f5c9e
DEEPKIRANC <cdeepkiran14@gmail.com>
2017-08-24 20:03:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,13 @@ console.log('Loaded!');
 var element=document.getElementById('hol');
 element.innerHTML="lol";
 var img=document.getElementById("madi");
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+5;
+    img.style.marginLeft=marginLeft + "px";
+}
 img.onclick=function(){
-    
-    img.style.marginLeft="100px";
+    var Interval=setInterval(moveRight,50);
     
 }
